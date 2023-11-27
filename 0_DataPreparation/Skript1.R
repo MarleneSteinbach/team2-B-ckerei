69bffa3c3f48a0a7596b538281bd199865b4b726
MarleneSteinbach <leni.mhs@gmail.com>
2023-11-27 10:01:21 +0100

    data preparation skript take 1

---
+++ 0_DataPreparation/Skript1.R
@@ -0,0 +1,15 @@
+library(styler)
+library(dplyr)
+setwd("")
+wetter <- read.table("wetter.csv", head=TRUE, sep = ",")
+umdat <- read.csv("umsatzdaten_gekuerzt.csv")
+kiwo <- read.csv("kiwo.csv")
+alle <- full_join(umdat, wetter, by = "Datum") %>%
+  full_join(kiwo, by = "Datum")
+as_tibble(alle)
+nrow(umdat)
+
+nrow(alle)
+alle_gefiltert <- alle %>%
+  filter(!is.na(Umsatz))
+nrow(alle_gefiltert)
