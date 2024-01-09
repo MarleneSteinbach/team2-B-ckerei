8b5bdbc1b4c907975bb44cddd58c74decd2b1193
R19-cmd <stu240121@mail.uni-kiel.de>
2024-01-09 17:26:05 +0100

    trainingsdatensatz mit allen daten kommentare entfernen wenn anderes spliting gefragt ist

--- 0_DataPreparation/Datenvorbereitung.R
+++ 0_DataPreparation/Datenvorbereitung.R
@@ -50,7 +50,7 @@ data <- data %>%
 ### Data Preparation ####
 
 # Preparation of independent variables ('features') by dummy coding the categorical variables
-features <- as_tibble(model.matrix(Umsatz ~ Wettercode + Bewoelkung + Windgeschwindigkeit + Temperatur + as.factor(Jahreszeit) + as.factor(Temperaturkategorie) + as.factor(Warengruppe) + Fussballspiel + Ferien + Feiertag  + KielerWoche + as.factor(Wochentag) + Flohmarkt + Kreuzfahrtschiffe + Handballspiele + Diff_wind + Diff_bewoelkung +Diff_temp,data))
+features <- as_tibble(model.matrix(Umsatz ~ Wettercode + Bewoelkung + Windgeschwindigkeit + Temperatur + as.factor(Jahreszeit) + as.factor(Temperaturkategorie) + as.factor(Warengruppe) + Fussballspiel + Ferien + Feiertag  + KielerWoche + as.factor(Wochentag) + Flohmarkt + Kreuzfahrtschiffe + Handballspiele + Diff_wind  + Diff_temp,data))
 names(features)
 features <- cbind(features, Datum =data$Datum , id=data$id )
 
@@ -68,11 +68,11 @@ prepared_data <- tibble(label=data$Umsatz, features) %>%  # inclusion of the dep
 
 # Train dataset (01.07.2013 to 31.07.2017)
 train_data <- prepared_data %>%
-  filter(Datum >= as.Date("2013-07-01") & Datum <= as.Date("2017-07-31"))
+  filter(Datum >= as.Date("2013-07-01") & Datum <= as.Date("2018-07-31"))
 
 # Validation dataset (01.08.2017 to 31.07.2018)
-valid_data <- prepared_data %>%
-  filter(Datum >= as.Date("2017-08-01") & Datum <= as.Date("2018-07-31"))
+#valid_data <- prepared_data %>%
+#  filter(Datum >= as.Date("2017-08-01") & Datum <= as.Date("2018-07-31"))
 
 # Test dataset (01.08.2018 and onwards, assuming you want the remaining data for testing)
 test_data <- prepared_data %>%
@@ -83,8 +83,8 @@ training_features <- select(train_data, -c(label,id, Datum))
 training_labels <- select(train_data, label)
 
 # Extract features and labels for validation
-validation_features <- select(valid_data, -c(label,id, Datum))
-validation_labels <- select(valid_data, label)
+#validation_features <- select(valid_data, -c(label,id, Datum))
+#validation_labels <- select(valid_data, label)
 
 # Extract features and labels for test
 test_features <- select(test_data, -c(label, Datum))
@@ -94,23 +94,22 @@ test_labels <- select(test_data, label)
 
 # Check the number of rows in each dataset
 nrow_train <- nrow(training_features)
-nrow_valid <- nrow(validation_features)
+#nrow_valid <- nrow(validation_features)
 nrow_test <- nrow(test_features)
 
 # Print the number of rows in each dataset
 cat("Number of rows in training dataset:", nrow_train, "\n")
-cat("Number of rows in validation dataset:", nrow_valid, "\n")
+#cat("Number of rows in validation dataset:", nrow_valid, "\n")
 cat("Number of rows in test dataset:", nrow_test, "\n")
 
 # Check the dimensions of the dataframes
 cat("Training features dimensions:", dim(training_features), "\n")
-cat("Validation features dimensions:",
-    dim(validation_features),
-    "\n")
+#cat("Validation features dimensions:",
+   # dim(validation_features), "\n")
 cat("Test features dimensions:", dim(test_features), "\n")
 cat("\n")
 cat("Training labels dimensions:", dim(training_labels), "\n")
-cat("Validation labels dimensions:", dim(validation_labels), "\n")
+#cat("Validation labels dimensions:", dim(validation_labels), "\n")
 cat("Test labels dimensions:", dim(test_labels), "\n")
 
 
@@ -124,8 +123,8 @@ subdirectory <- "csv_data"
 
 # Export of the prepared data to subdirectory
 write_csv(training_features, paste0(subdirectory, "/training_features.csv"))
-write_csv(validation_features, paste0(subdirectory, "/validation_features.csv"))
+#write_csv(validation_features, paste0(subdirectory, "/validation_features.csv"))
 write_csv(test_features, paste0(subdirectory, "/test_features.csv"))
 write_csv(training_labels, paste0(subdirectory, "/training_labels.csv"))
-write_csv(validation_labels, paste0(subdirectory, "/validation_labels.csv"))
+#write_csv(validation_labels, paste0(subdirectory, "/validation_labels.csv"))
 write_csv(test_labels, paste0(subdirectory, "/test_labels.csv"))
